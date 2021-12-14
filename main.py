a6f5e58b63efc165deacfeec13fde276f6a0c5e7
Denis <denis.kr.pr@gmail.com>
2021-12-14 18:06:46 +0200

    finished 2 task

--- main.py
+++ main.py
@@ -1,34 +1,31 @@
-from transliterate import translit
+matrix = [[1, 3, 3, 4], [2, 1, 3, 5], [4, 0, 1, 7], [5, 2, 1, 0], [0, 4, 8, 3]]
 
+# first task
+sorted_array = []
+for i in range(len(matrix)):
+    sorted_array.append(matrix[i][1])
+sorted_array.sort()
+print(sorted_array)
 
-def convert_word_to_amt(word):
-    amt = 0
-    for char in word:
-        if char.isalnum():
-            amt += ord(char)
-        amt += 0
-    return amt
+# second task
+for i in range(len(matrix)):
+    del(matrix[i][1])
+dict_from_matrix = dict(zip(sorted_array, matrix))
+print(dict_from_matrix)
 
+# third task
+sorted_dict = {key: sorted(
+    dict_from_matrix[key], reverse=True
+) for key in sorted(dict_from_matrix)}
+print(sorted_dict)
 
-def vectorizer():
-    with open('text.txt', 'r', encoding='utf-8') as file:
-        sentences = [sentence for sentence in translit(
-            str(file.read()), language_code='uk', reversed=True
-        ).replace('\r', '').split('.') if sentence != '']
-    words_in_sentence = []
-    for sentence in sentences:
-        words_in_sentence.append(sentence.split())
-    resault = [[]] * len(words_in_sentence)
-    for i in range(len(words_in_sentence)):
-       for word in words_in_sentence[i]:
-           resault[i].append(convert_word_to_amt(word))
-    file.close()
-    return resault
+# fourth task
+set_from_dict_values = set()
+for i in sorted_dict.values():
+    set_from_dict_values.update(i)
+print(set_from_dict_values)
 
-
-def main():
-    print(vectorizer())
-
-
-if __name__ == '__main__':
-    main()
+# fifth task
+set_to_str = ", ".join(map(str, set_from_dict_values))
+print(type(set_to_str))
+print(set_to_str)
