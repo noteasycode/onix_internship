3e2740c724686f3a6783fdc4ad05fa3ead5b1765
Denis <denis.kr.pr@gmail.com>
2021-12-17 12:56:08 +0200

    finished 3-d task

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
-matrix = [[1, 3, 3, 4], [2, 1, 3, 5], [4, 0, 1, 7], [5, 2, 1, 0], [0, 4, 8, 3]]
+import json
+import pandas as pd
 
-# first task
-sorted_array = []
-for i in range(len(matrix)):
-    sorted_array.append(matrix[i][1])
-sorted_array.sort()
-print(sorted_array)
 
-# second task
-for i in range(len(matrix)):
-    del(matrix[i][1])
-dict_from_matrix = dict(zip(sorted_array, matrix))
-print(dict_from_matrix)
+# task_1
+def f1(x):
+    return x/(x+100)
 
-# third task
-sorted_dict = {key: sorted(
-    dict_from_matrix[key], reverse=True
-) for key in sorted(dict_from_matrix)}
-print(sorted_dict)
 
-# fourth task
-set_from_dict_values = set()
-for i in sorted_dict.values():
-    set_from_dict_values.update(i)
-print(set_from_dict_values)
+def f2(x):
+    return 1 / x
 
-# fifth task
-set_to_str = ", ".join(map(str, set_from_dict_values))
-print(type(set_to_str))
-print(set_to_str)
+
+# task_2
+def f3(x):
+    return 20 * (f1(x) + f2(x)) / x
+
+
+# task_3
+dic = {x: [f1(x), f2(x), f3(x)] for x in range(5, 90, 1)}
+
+# task_4
+csv_columns = ['x', 'f1(x)', 'f2(x)', 'f3(x)']
+pd.DataFrame(dic).T.reset_index().to_csv('csvfile.csv', header=csv_columns, index=False)
+
+# task_5
+df = pd.read_csv('csvfile.csv', delimiter=',')
+list_of_rows = [list(row) for row in df.values]
+print(list_of_rows)
+
+# task_6
+with open('data.json', 'w') as file:
+    json.dump(list_of_rows, file, indent=4)
